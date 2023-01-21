bc500aea0858239d792d71f9b82898e77acace88
manu_ <manu_@LAPTOP-7HTT7HMN>
2023-01-21 11:59:31 -0600

    21:0:00

---
+++ learn-to-code-with-ruby/01 - Introduction & Installation/hello_world.rb
@@ -0,0 +1,21 @@
+#this is a comment
+puts "Hello World"
+puts 4
+
+#code runner
+#IRB Interactive Ruby : irb : in the terminal : REPL : used to write ruby in the terminal : nil represents that it gives nothing back, just prints. 
+#exit or quit to finish using IRB
+
+
+
+
+
+
+
+
+
+
+
+
+
+
